08e644f58e12e21d7b68c67b109ddef20e6ec5a7
Rebekah <rebekahlow.jy@u.nus.edu>
2018-02-28 20:39:53 +0800

    Add users endpoints

---
+++ controllers/queryHandler.js
@@ -0,0 +1,151 @@
+console.log('QueryHandler Initialized');
+
+const Sequelize = require('sequelize');
+const selectClause =   {type: Sequelize.QueryTypes.SELECT};
+const sequelize = new Sequelize('collab', 'root', '12341234', {
+  host: 'localhost',
+  dialect: 'mysql',
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
+
+sequelize
+  .authenticate()
+  .then(function(err) {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(function (err) {
+    console.log('Unable to connect to the database.');
+  });
+
+
+exports.getUsersCount = function (req, res) {
+  const query = 'SELECT COUNT(*) as count FROM users;';
+  sequelize.query(query, selectClause)
+  .then((result) => {
+    const count = result[0].count;
+    res.send({
+  	  count: count
+    });
+  })
+  .catch(function (err) {
+    res.status(400).send('Error ' + err);
+  });
+}
+
+exports.getMilestonesCount = function (req, res) {
+  const query = 'SELECT COUNT(*) as count FROM milestones;';
+  sequelize.query(query, selectClause)
+  .then((result) => {
+    const count = result[0].count;
+    res.send({
+  	  count: count
+    });
+  })
+  .catch(function (err) {
+    res.status(400).send('Error ' + err);
+  });
+}
+
+/*
+  PARAMS
+  startDate: YYYY-MM-DD (e.g 2011-09-14)
+  endDate: YYYY-MM-DD (e.g 2011-09-14)
+  maxUsers INT
+*/
+exports.getNumUsersCreatedBetweenDates = function (req, res) {
+  const startDate = req.body.startDate;
+  const endDate = req.body.endDate;
+
+  const datedUsersQuery = 'SELECT COUNT (*) as count FROM users '
+  + 'WHERE created_at between \'' + startDate + '\' AND \'' + endDate + '\''
+  + ';';
+
+  sequelize.query(datedUsersQuery, selectClause)
+    .then((users) => {
+      res.send(users[0]);
+  })
+  .catch(function (err) {
+    res.status(400).send('Error ' + err);
+  });
+};
+
+exports.getNumUsersUpdatedBetweenDates = function (req, res) {
+  const startDate = req.body.startDate;
+  const endDate = req.body.endDate;
+
+  const datedUsersQuery = 'SELECT COUNT (*) as count FROM users '
+  + 'WHERE updated_at between \'' + startDate + '\' AND \'' + endDate + '\''
+  + ';';
+
+  sequelize.query(datedUsersQuery, selectClause)
+    .then((users) => {
+      res.send(users[0]);
+  })
+  .catch(function (err) {
+    res.status(400).send('Error ' + err);
+  });
+};
+
+exports.getLatestUsers = function (req, res) {
+  let maxUsers = req.body.maxUsers;
+  if (!maxUsers) maxUsers = 10;
+
+  const datedUsersQuery =
+  'SELECT u.display_name, u.email, u.github_login, u.created_at, GROUP_CONCAT(content) as user_projects '
+  + ' FROM users u'
+  + ' JOIN user_projects up ON up.user_id = u.id'
+  + ' JOIN projects p ON up.project_id = p.id'
+  + ' GROUP BY u.id'
+  + ' ORDER BY u.created_at DESC LIMIT ' + maxUsers
+  + ';';
+
+  sequelize.query(datedUsersQuery, selectClause)
+    .then((datedUsers) => {
+      res.send(datedUsers);
+  })
+  .catch(function (err) {
+    res.status(400).send('Error ' + err);
+  });
+};
+
+
+
+// Project Related
+
+exports.getProjectsCount = function (req, res) {
+  const query = 'SELECT COUNT(*) as count FROM projects;';
+  sequelize.query(query, selectClause).then((result) => {
+    res.send(result[0]);
+  })
+  .catch(function (err) {
+    res.status(400).send('Error ' + err);
+  });
+}
+
+exports.getLatestProjects = function (req, res) {
+  let maxProjects = req.body.maxProjects;
+  if (!maxProjects) maxProjects = 10;
+
+  const query =
+  'SELECT p.content, p.github_repo_name, p.created_at, GROUP_CONCAT(display_name) as members '
+  + ' FROM projects p'
+  + ' JOIN user_projects up ON up.project_id = p.id'
+  + ' JOIN users u ON up.user_id = u.id'
+  + ' GROUP BY p.id'
+  + ' ORDER BY p.created_at DESC LIMIT ' + maxProjects
+  + ';';
+
+  sequelize.query(query, selectClause)
+    .then((result) => {
+      res.send(result);
+  })
+  .catch(function (err) {
+    res.status(400).send('Error ' + err);
+  });
+};
--- server.js
+++ server.js
@@ -1,114 +1,50 @@
 console.log('Collab Dashboard Backend Starting V0.1');
 
-var express = require('express');
-var cors = require('cors');
-var bodyParser = require('body-parser');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('collab', 'root', '12341234', {
-  host: 'localhost',
-  dialect: 'mysql',
-
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-});
-
-sequelize
-  .authenticate()
-  .then(function(err) {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(function (err) {
-    console.log('Unable to connect to the database.');
-  });
+const queryHandler = require('./controllers/queryHandler');
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ type: 'application/*+json' }));
 
-// use it before all route definitions
-app.use(cors({origin: 'http://localhost:3000'}));
+// // use it before all route definitions
+// app.use(cors({origin: 'http://localhost:3000'}));
 
 app.get('/', function (req, res) {
   res.send('Dashboard Backend Root')
 })
 
-app.get('/users-count', function (req, res) {
-  sequelize.query('SELECT COUNT(*) as count FROM users;').then((result) => {
-    const count = result[0][0].count;
-    res.send({
-  	  count: count
-    });
-  });
-})
+/******************************
+User Routes
+*******************************/
+app.route('/users/count')
+    .get(queryHandler.getUsersCount);
 
-app.get('/milestones-count', function (req, res) {
-  sequelize.query('SELECT COUNT(*) as count FROM milestones;').then((result) => {
-    const count = result[0][0].count;
-    res.send({
-  	  count: count
-    });
-  });
-})
+app.route('/users/num-created-between-dates')
+    .post(queryHandler.getNumUsersCreatedBetweenDates);
 
-/*
-  PARAMS
-  startDate: YYYY-MM-DD (e.g 2011-09-14)
-  endDate: YYYY-MM-DD (e.g 2011-09-14)
-  maxUsers INT
-*/
+app.route('/users/num-updated-between-dates')
+    .post(queryHandler.getNumUsersUpdatedBetweenDates);
 
-const selectClause =   {type: Sequelize.QueryTypes.SELECT};
+app.route('/users/latest')
+    .post(queryHandler.getLatestUsers);
 
-app.post('/users/num-created-between-dates', function (req, res) {
-  const startDate = req.body.startDate;
-  const endDate = req.body.endDate;
+/******************************
+Milestone Routes
+*******************************/
+app.route('/milestones/count')
+    .get(queryHandler.getMilestonesCount);
 
-  const datedUsersQuery = 'SELECT COUNT (*) as count FROM users '
-  + 'WHERE created_at between \'' + startDate + '\' AND \'' + endDate + '\''
-  + ';';
+/******************************
+Projects Routes
+*******************************/
+app.route('/projects/count')
+    .get(queryHandler.getProjectsCount);
 
-  sequelize.query(datedUsersQuery, selectClause)
-    .then((users) => {
-      res.send(users[0]);
-    });
-});
-
-app.post('/users/num-updated-between-dates', function (req, res) {
-  const startDate = req.body.startDate;
-  const endDate = req.body.endDate;
-
-  const datedUsersQuery = 'SELECT COUNT (*) as count FROM users '
-  + 'WHERE updated_at between \'' + startDate + '\' AND \'' + endDate + '\''
-  + ';';
-
-  sequelize.query(datedUsersQuery, selectClause)
-    .then((users) => {
-      res.send(users[0]);
-    });
-});
-
-app.post('/users/get-latest', function (req, res) {
-  let maxUsers = req.body.maxUsers;
-  if (!maxUsers) maxUsers = 10;
-
-  const datedUsersQuery =
-  'SELECT u.display_name, u.email, u.github_login, u.created_at, GROUP_CONCAT(content) as user_projects '
-  + ' FROM users u'
-  + ' JOIN user_projects up ON up.user_id = u.id'
-  + ' JOIN projects p ON up.project_id = p.id'
-  + ' GROUP BY u.id'
-  + ' ORDER BY u.created_at DESC LIMIT ' + maxUsers
-  + ';';
-
-  sequelize.query(datedUsersQuery, selectClause)
-    .then((datedUsers) => {
-      res.send(datedUsers);
-    });
-});
+app.route('/projects/latest')
+    .post(queryHandler.getLatestProjects);
 
 app.listen(3001)
