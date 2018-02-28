cdc85b23f7d2748fb1c23c468e81c18ebb9c5062
Rebekah <rebekahlow.jy@u.nus.edu>
2018-02-28 17:32:48 +0800

    Create USER routes : users-count, num-created-between-dates, num-updated-between-dates, get-latest

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ console.log('Collab Dashboard Backend Starting V0.1');
 
 var express = require('express');
 var cors = require('cors');
+var bodyParser = require('body-parser');
 
 const Sequelize = require('sequelize');
-
 const sequelize = new Sequelize('collab', 'root', '12341234', {
   host: 'localhost',
   dialect: 'mysql',
@@ -26,20 +26,18 @@ sequelize
     console.log('Unable to connect to the database.');
   });
 
-// sequelize.query("SHOW TABLES;")
-//   .then(users => {
-//     console.log(JSON.stringify(users));
-//   });
-
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ type: 'application/*+json' }));
+
 // use it before all route definitions
 app.use(cors({origin: 'http://localhost:3000'}));
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Dashboard Backend Root')
 })
 
-app.get('/projects-count', function (req, res) {
+app.get('/users-count', function (req, res) {
   sequelize.query('SELECT COUNT(*) as count FROM users;').then((result) => {
     const count = result[0][0].count;
     res.send({
@@ -56,5 +54,61 @@ app.get('/milestones-count', function (req, res) {
     });
   });
 })
+
+/*
+  PARAMS
+  startDate: YYYY-MM-DD (e.g 2011-09-14)
+  endDate: YYYY-MM-DD (e.g 2011-09-14)
+  maxUsers INT
+*/
+
+const selectClause =   {type: Sequelize.QueryTypes.SELECT};
+
+app.post('/users/num-created-between-dates', function (req, res) {
+  const startDate = req.body.startDate;
+  const endDate = req.body.endDate;
+
+  const datedUsersQuery = 'SELECT COUNT (*) as count FROM users '
+  + 'WHERE created_at between \'' + startDate + '\' AND \'' + endDate + '\''
+  + ';';
+
+  sequelize.query(datedUsersQuery, selectClause)
+    .then((users) => {
+      res.send(users[0]);
+    });
+});
+
+app.post('/users/num-updated-between-dates', function (req, res) {
+  const startDate = req.body.startDate;
+  const endDate = req.body.endDate;
+
+  const datedUsersQuery = 'SELECT COUNT (*) as count FROM users '
+  + 'WHERE updated_at between \'' + startDate + '\' AND \'' + endDate + '\''
+  + ';';
+
+  sequelize.query(datedUsersQuery, selectClause)
+    .then((users) => {
+      res.send(users[0]);
+    });
+});
+
+app.post('/users/get-latest', function (req, res) {
+  let maxUsers = req.body.maxUsers;
+  if (!maxUsers) maxUsers = 10;
+
+  const datedUsersQuery =
+  'SELECT u.display_name, u.email, u.github_login, u.created_at, GROUP_CONCAT(content) as user_projects '
+  + ' FROM users u'
+  + ' JOIN user_projects up ON up.user_id = u.id'
+  + ' JOIN projects p ON up.project_id = p.id'
+  + ' GROUP BY u.id'
+  + ' ORDER BY u.created_at DESC LIMIT ' + maxUsers
+  + ';';
+
+  sequelize.query(datedUsersQuery, selectClause)
+    .then((datedUsers) => {
+      res.send(datedUsers);
+    });
+});
 
 app.listen(3001)
