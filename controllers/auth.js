138f5a81274f3e86f0c3d3ec3afc14d53a76633f
Rebekah <rebekahlow.jy@u.nus.edu>
2018-03-07 23:35:52 +0800

    Fix runtime errors

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,24 @@
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const LocalStrategy = require('passport-local').Strategy;
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 const config = {
   jwtSecret: 'mySecret',
-  jwtExpiresInSeconds: '600000'
+  jwtExpiresInSeconds: (60 * 60 * 2)
 }
+// const googleCred = {
+//   "client_id": "183936930394-i706s7cn9fttmhqv7dh7rkqs3nkod37f.apps.googleusercontent.com",
+//   "client_secret": "uXl3k4jApfY2Qp-7P0rzUDxL"
+// }
 
-//Local Login for forms, where username & password are provided in body
+/***************************************************************
+  Local Login for form submissions
+  @Params username (in body)
+  @Params password (in body)
+  ***************************************************************/
 passport.use('local-login', new LocalStrategy({
     // by default, local strategy uses username and password, we will override with username
     usernameField : 'username',
@@ -35,12 +44,15 @@ const jwtStrategyOptions = {
   jwtFromRequest : ExtractJwt.fromAuthHeaderWithScheme("jwt"),
   secretOrKey : config.jwtSecret
 };
-//Jwt Authentication to authenticate using header data
-/* Example Header Data =
-{
-  Authorization: JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InJlYmVrYWgiLCJpYXQiOjE1MjAzNTEwNDYsImV4cCI6MTUyMDM1MTY0Nn0.fqUDuz72G8GJmWTVc4E2u6Aq5G7gdosOAEC2c3D9pvY
-} */
-passport.use('jwt-login', new JwtStrategy(jwtStrategyOptions,
+
+/***************************************************************
+  Jwt Authentication to authenticate using header data
+  Example Header Data =
+  {
+    Authorization: JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InJlYmVrYWgiLCJpYXQiOjE1MjAzNTEwNDYsImV4cCI6MTUyMDM1MTY0Nn0.fqUDuz72G8GJmWTVc4E2u6Aq5G7gdosOAEC2c3D9pvY
+  }
+  ***************************************************************/
+passport.use('jwt-auth', new JwtStrategy(jwtStrategyOptions,
   function(jwt_payload, done_callback) {
     const user = {
       username: jwt_payload.username
@@ -53,6 +65,21 @@ passport.use('jwt-login', new JwtStrategy(jwtStrategyOptions,
     }
   }
 ));
+
+/***************************************************************
+  Google Login Strategy
+  ***************************************************************/
+// passport.use('google-login', new GoogleStrategy({
+//     consumerKey: googleCred.client_id,
+//     consumerSecret: googleCred.client_secret,
+//     callbackURL: "http://www.example.com/auth/google/callback"
+//   },
+//   function(token, tokenSecret, profile, done) {
+//       User.findOrCreate({ googleId: profile.id }, function (err, user) {
+//         return done(err, user);
+//       });
+//   }
+// ));
 
 passport.serializeUser(function(user, done) {
   // console.log('serializeUser ' + JSON.stringify(user));
@@ -80,4 +107,6 @@ exports.login = function(req, res, next) {
 };
 
 exports.localAuthenticated = passport.authenticate('local-login');
-exports.jwtAuthenticated = passport.authenticate('jwt-login');
+exports.jwtAuthenticated = passport.authenticate('jwt-auth');
+// exports.googleAuthenticated = passport.authenticate('google-auth');
+exports.passport = passport;
--- server.js
+++ server.js
@@ -3,12 +3,13 @@ console.log('Collab Dashboard Backend Starting V0.1');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const passport = require('passport');
 
 const authController = require('./controllers/auth');
 const usersController = require('./controllers/users');
 const projectsController = require('./controllers/projects');
 const milestonesController = require('./controllers/milestones');
+
+const passport = authController.passport;
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -28,13 +29,25 @@ app.get('/', function (req, res) {
   res.send('Dashboard Backend Root')
 })
 
-
+/******************************
+  Auth Routes
+********************************/
 app.route('/login')
     .post(authController.localAuthenticated, authController.login);
 
-/ ******************************
+// app.get('/auth/google',
+//   passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/userinfo.profile'] }));
+//
+// app.get('/auth/google/callback',
+//   passport.authenticate('google', { failureRedirect: '/login' }),
+//   function(req, res) {
+//     res.redirect('/');
+//   });
+
+
+/******************************
   User Routes
-******************************** /
+********************************/
 app.route('/users/count')
     .get(authController.jwtAuthenticated, usersController.getUsersCount);
 
@@ -68,9 +81,9 @@ app.route('/users/retention-rate')
 app.route('/users/latest')
     .post(usersController.getLatestUsers);
 
-/ ******************************
+/*****************************
   Milestone Routes
-******************************** /
+********************************/
 app.route('/milestones/count')
     .get(milestonesController.getMilestonesCount);
 
@@ -78,9 +91,9 @@ app.route('/milestones/completed-count')
     .get(milestonesController.getCompletedMilestonesCount);
 
 
-/ ******************************
+/******************************
   Projects Routes
-******************************** /
+********************************/
 app.route('/projects/count')
     .get(projectsController.getProjectsCount);
 
